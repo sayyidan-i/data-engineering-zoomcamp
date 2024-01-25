af46f2a118cac091d5a234b91661bf1c9896b602
sayyidan-i <sayyidan2020@gmail.com>
2024-01-25 20:42:18 +0700

    Week 1 finish

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_basic/main.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_basic/main.tf
@@ -2,47 +2,32 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.51.0"
+      version = "5.13.0"
     }
   }
 }
 
 provider "google" {
-# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
-#  credentials = 
-  project = "<Your Project ID>"
+  # Configuration options
+  project = "zoocamp-de"
   region  = "us-central1"
 }
 
-
-
-resource "google_storage_bucket" "data-lake-bucket" {
-  name          = "<Your Unique Bucket Name>"
+resource "google_storage_bucket" "terraform-bucket" {
+  name          = "zoocamp-de-250124"
   location      = "US"
-
-  # Optional, but recommended settings:
-  storage_class = "STANDARD"
-  uniform_bucket_level_access = true
-
-  versioning {
-    enabled     = true
-  }
+  force_destroy = true
 
   lifecycle_rule {
-    action {
-      type = "Delete"
-    }
     condition {
-      age = 30  // days
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
     }
   }
-
-  force_destroy = true
 }
 
-
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = "<The Dataset Name You Want to Use>"
-  project    = "<Your Project ID>"
-  location   = "US"
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = "demo_dataset"
 }
